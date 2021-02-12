20636419afa61de71ba6aeec34a26a844645d5db
Anton Melyakov <amelyakov@intracol.com>
2021-02-12 14:09:33 +0200

    Empty deadline bug fixed

--- list.php
+++ list.php
@@ -39,11 +39,18 @@ if(isset($_SESSION['msg']) && $_SESSION['msg'] != ''){
             <tbody>
                 <?php
                 foreach ($all_tasks as $task) {
+                    //check icon if task is done
                     $task_done = $task->task_done ? $check_icon : "";
+                    //empty value if deadline is not set
+                    if(!strtotime($task->deadline) || (strtotime($task->deadline) < 0)){
+                        $task_deadline = '';
+                    }else{
+                        $task_deadline =  date_format(date_create($task->deadline), "d-m-Y"); 
+                    }
                     print("<tr> 
       <th scope='row'>" . $task->task . "</th>
       <th scope='row' class='text-center'>" . $task_done  . " </th>
-      <th scope='row'>" . date_format(date_create($task->deadline), "d-m-Y") . "</th> 
+      <th scope='row'>" . $task_deadline . "</th> 
       <th scope='row' class='text-center'> <a class='btn btn-primary' href='edit.php?id=" . $task->id . "'>edit</a></th>  
       <th scope='row' class='text-center'> <a class='btn btn-primary' href='remove.php?id=" . $task->id . "'>remove</a></th>      
      </tr>");
